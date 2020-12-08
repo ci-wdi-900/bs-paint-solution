4f02a1333f6acd985ad3a3a57cf8e8cdf2c55d3d
Colin Jaffe <balloonasaurus@gmail.com>
2020-12-08 15:50:18 -0500

    Adds stretch goal solution.

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
+let mouseDown = false;
+
 function getColor(element) {
-  return element.classList[1];
+    return element.classList[1];
 }
 
 function handleClickSquare (event) {
-  const square = event.target;
-  const brush = document.querySelector('.current-brush');
-  square.classList.replace(getColor(square), getColor(brush));
+    const square = event.target;
+    const brush = document.querySelector('.current-brush');
+    square.classList.replace(getColor(square), getColor(brush));
+    mouseDown = false;
+}
+
+function handleMouseOverSquare (event) {
+  if (mouseDown) {
+    const square = event.target;
+    const brush = document.querySelector('.current-brush');
+    square.classList.replace(getColor(square), getColor(brush));
+  }
 }
 
 const squares = document.querySelectorAll('.square')
 
 for (const square of squares) {
+  square.addEventListener('mouseenter', handleMouseOverSquare)
   square.addEventListener('click', handleClickSquare)
 };
 
@@ -24,3 +36,11 @@ const paletteColors = document.querySelectorAll('.palette-color');
 for (const paletteColor of paletteColors) {
   paletteColor.addEventListener('click', handleClickPaletteColor);
 };
+
+document.body.addEventListener('mousedown', () => {
+  mouseDown = true;
+})
+
+document.body.addEventListener('mouseup', () => {
+  mouseDown = false;
+})
